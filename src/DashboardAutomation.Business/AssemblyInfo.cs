270d2953c6618a2e5c1d123c39d1a397e4042b17
.:PaRsA:. <parsa.eng01@gmail.com>
2026-06-08 14:59:31 +0330

    update [business]: add AssemblyInfo class with global using directives

---
+++ src/DashboardAutomation.Business/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+
+global using DashboardAutomation.Common;
+global using DashboardAutomation.Shared;
+
+global using System.Diagnostics;
+
+using System.Reflection;
+
+namespace DashboardAutomation.Business;
+
+public static class AssemblyInfo
+{
+  public static Assembly Assembly => typeof(AssemblyInfo).Assembly;
+}
